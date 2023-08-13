9aed7b25d80b655fe6cb6af082205b8ad4933a7e
RomanIgambaev <roman.igambaev11@gmail.com>
2023-08-13 11:28:15 +0500

    More less equal

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        try {
+            System.out.println("Привет, я сравниваю два числа введи первое число");
+            int fir;
+            int sec;
+            fir = scanner.nextInt();
+            System.out.println("Твоё число " + fir + " введи второе число");
+            sec = scanner.nextInt();
+            if (fir > sec) {
+                System.out.println(fir + ">" + sec);
+            } else if (fir < sec) {
+                System.out.println(fir + "<" + sec);
+            } else if (fir == sec) {
+                System.out.println(fir + "=" + sec);
+            }
+        }catch (Exception ex){
+            System.out.println("Ты ввёл что-то не так");
+        }
     }
 }
